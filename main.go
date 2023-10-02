85de29a2788a9c98ec6fb7be88cf278aa50a2c2e
jaytrairat <119471302+jaytrairat@users.noreply.github.com>
2023-10-02 18:57:55 +0700

    feat: renew folder

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	isRenew bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "extract-apks",
 	Short: "Extract APK files from a directory",
@@ -23,9 +27,14 @@ func run(cmd *cobra.Command, args []string) {
 	currentDir, _ := os.Getwd()
 
 	filepath.Walk(targetDirectory, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() && path != scriptDir && path != currentDir && !strings.HasPrefix(filepath.Base(path), ".") && filepath.Dir(path) == targetDirectory {
+		if info.IsDir() && path != scriptDir && path != currentDir && !strings.HasPrefix(filepath.Base(path), ".") && filepath.Base(path) != "templates" && filepath.Dir(path) == targetDirectory {
 			codeExtractFolder := filepath.Join(path, "CodeExtract")
 			androidProjectFolder := filepath.Join(path, "AndroidProject")
+
+			if isRenew {
+				os.RemoveAll(codeExtractFolder)
+				os.RemoveAll(androidProjectFolder)
+			}
 			os.MkdirAll(codeExtractFolder, os.ModePerm)
 			os.MkdirAll(androidProjectFolder, os.ModePerm)
 			fmt.Printf("%s :: Folders created inside %s\n", time.Now().Format("2006-01-02 15:04:05"), path)
@@ -46,6 +55,7 @@ func run(cmd *cobra.Command, args []string) {
 }
 
 func main() {
+	rootCmd.Flags().BoolVarP(&isRenew, "renew", "r", false, "remove old folders")
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
