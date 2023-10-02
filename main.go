cb4927b1ff162e8c0b4be532b62ce690b49048ff
jaytrairat <jaytrairat@outlook.com>
2023-10-02 16:10:02 +0700

    feat: extract apk to code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
@@ -10,42 +11,27 @@ import (
 
 func main() {
 	targetDirectory := "."
+	scriptDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	currentDir, _ := os.Getwd()
 
-	scriptDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	currentDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	err = filepath.Walk(targetDirectory, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+	filepath.Walk(targetDirectory, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() && path != scriptDir && path != currentDir && !strings.HasPrefix(filepath.Base(path), ".") && filepath.Dir(path) == targetDirectory {
 			codeExtractFolder := filepath.Join(path, "CodeExtract")
 			androidProjectFolder := filepath.Join(path, "AndroidProject")
-
-			if err := os.MkdirAll(codeExtractFolder, os.ModePerm); err != nil {
-				return err
-			}
-			if err := os.MkdirAll(androidProjectFolder, os.ModePerm); err != nil {
-				return err
-			}
+			os.MkdirAll(codeExtractFolder, os.ModePerm)
+			os.MkdirAll(androidProjectFolder, os.ModePerm)
 			fmt.Printf("%s :: Folders created inside %s\n", time.Now().Format("2006-01-02 15:04:05"), path)
 		}
 		return nil
 	})
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	filepath.Walk(targetDirectory, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && filepath.Ext(info.Name()) == ".apk" {
+			cmd := exec.Command("jadx", "-d", filepath.Join(currentDir, filepath.Dir(path), "CodeExtract"), path)
+			cmd.CombinedOutput()
+		}
+		return nil
+	})
 
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + " :: Completed")
 }
